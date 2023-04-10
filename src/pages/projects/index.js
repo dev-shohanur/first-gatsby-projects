b0c52eb20540e7854334b0c0906642adfcfbd0c2
MD Shohanur Rahman <98300635+dvsrshohan@users.noreply.github.com>
2023-04-11 01:33:09 +0600

    Update CSS And GraphQL

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -10,13 +10,15 @@ export default function Projects({ data }) {
         <Layout>
             <section>
                 <div className="container">
-                    <h2>Projects</h2>
-                    <h5>This is Projects Page</h5>
+                    <div className='heading'>
+                        <h2>My Projects</h2>
+                        <h3>This is My All Projects Page</h3>
+                    </div>
                     <div className='projects'>
                         {projects.map(project => (
                             <Link to={'/projects/' + project.frontmatter.slug} key={project.id}>
                                 <div>
-                                    <img src={project.frontmatter.thumb} />
+                                    <img src={project.frontmatter.thumb} alt={project.frontmatter.title} />
                                     <h3>{project.frontmatter.title}</h3>
                                     <p>{project.frontmatter.stack}</p>
 
@@ -35,7 +37,7 @@ export default function Projects({ data }) {
 
 export const query = graphql`
 query projectsPage {
-    projects: allMarkdownRemark(sort: {frontmatter: {title: DESC}}) {
+    projects: allMarkdownRemark(sort: {frontmatter: {title: ASC}}) {
       nodes {
         frontmatter {
           slug
